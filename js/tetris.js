45dc01f8af62a5cac0838cfee3be539d1efe089d
Julian Barker <julian.c.barker@gmail.com>
2022-09-05 15:21:21 -0700

    fix indentation

--- js/tetris.js
+++ js/tetris.js
@@ -274,18 +274,18 @@ function S() {
         break;
       case 180:
         this.coords = [
-            [this.bottom[0] - 1, this.bottom[1]],
-            [this.bottom[0], this.bottom[1]],
-            [this.bottom[0], this.bottom[1] - 1],
-            [this.bottom[0] + 1, this.bottom[1] - 1]
+          [this.bottom[0] - 1, this.bottom[1]],
+          [this.bottom[0], this.bottom[1]],
+          [this.bottom[0], this.bottom[1] - 1],
+          [this.bottom[0] + 1, this.bottom[1] - 1]
         ];
         break;
       case 270:
         this.coords = [
-            [this.bottom[0] - 1, this.bottom[1] - 2],
-            [this.bottom[0] - 1, this.bottom[1] - 1],
-            [this.bottom[0], this.bottom[1] - 1],
-            [this.bottom[0], this.bottom[1]]
+          [this.bottom[0] - 1, this.bottom[1] - 2],
+          [this.bottom[0] - 1, this.bottom[1] - 1],
+          [this.bottom[0], this.bottom[1] - 1],
+          [this.bottom[0], this.bottom[1]]
         ];
         break;
       default:
@@ -303,96 +303,93 @@ function S() {
 
 
 function T() {
-    this.val = 1;
-    this.state = 0;
-    this.coords = [];
-    this.bottom = [5, 3];
-  
-    this.createCoords = function() {
-      switch(this.state) {
-        case 90:
-          this.coords = [
-            [this.bottom[0], this.bottom[1] - 2],
-            [this.bottom[0], this.bottom[1] - 1],
-            [this.bottom[0] + 1, this.bottom[1] - 1],
-            [this.bottom[0], this.bottom[1]]
-          ];
-          break;
-        case 180:
-          this.coords = [
-            [this.bottom[0] + 1, this.bottom[1] - 1],
-            [this.bottom[0], this.bottom[1] - 1],
-            [this.bottom[0], this.bottom[1]],
-            [this.bottom[0] + 1, this.bottom[1] - 1]
-          ];
-          break;
-        case 270:
-          this.coords = [
-            [this.bottom[0], this.bottom[1]],
-            [this.bottom[0], this.bottom[1] -1],
-            [this.bottom[0] - 1, this.bottom[1] - 1],
-            [this.bottom[0], this.bottom[1] - 2]
-          ];
-          break;
-        default:
-          this.coords = [
-            [this.bottom[0] - 1, this.bottom[1]],
-            [this.bottom[0], this.bottom[1]],
-            [this.bottom[0], this.bottom[1] - 1],
-            [this.bottom[0] + 1, this.bottom[1]]
-          ];
-      }
-    };
-  
+  this.val = 1;
+  this.state = 0;
+  this.coords = [];
+  this.bottom = [5, 3];
 
-    this.createCoords();
+  this.createCoords = function() {
+    switch(this.state) {
+      case 90:
+        this.coords = [
+          [this.bottom[0], this.bottom[1] - 2],
+          [this.bottom[0], this.bottom[1] - 1],
+          [this.bottom[0] + 1, this.bottom[1] - 1],
+          [this.bottom[0], this.bottom[1]]
+        ];
+        break;
+      case 180:
+        this.coords = [
+          [this.bottom[0] + 1, this.bottom[1] - 1],
+          [this.bottom[0], this.bottom[1] - 1],
+          [this.bottom[0], this.bottom[1]],
+          [this.bottom[0] + 1, this.bottom[1] - 1]
+        ];
+        break;
+      case 270:
+        this.coords = [
+          [this.bottom[0], this.bottom[1]],
+          [this.bottom[0], this.bottom[1] -1],
+          [this.bottom[0] - 1, this.bottom[1] - 1],
+          [this.bottom[0], this.bottom[1] - 2]
+        ];
+        break;
+      default:
+        this.coords = [
+          [this.bottom[0] - 1, this.bottom[1]],
+          [this.bottom[0], this.bottom[1]],
+          [this.bottom[0], this.bottom[1] - 1],
+          [this.bottom[0] + 1, this.bottom[1]]
+        ];
+    }
+  };
+
+
+  this.createCoords();
 }
 
-const piece = new T();
-console.log(piece);
-
 function Z() {
-    this.val = 1;
-    this.state = 0;
-    this.coords = [];
-    this.bottom = [5, 3];
-  
-    this.createCoords = function() {
-      switch(this.state) {
-        case 90:
-          this.coords = [
-            [this.bottom[0] + 1, this.bottom[1] - 2],
-            [this.bottom[0] - 1, this.bottom[1] - 1],
-            [this.bottom[0], this.bottom[1] - 1],
-            [this.bottom[0], this.bottom[1]]
-          ];
-          break;
-        case 180:
-          this.coords = [
-            [this.bottom[0] - 1, this.bottom[1] - 2],
-            [this.bottom[0] - 1, this.bottom[1] - 1],
-            [this.bottom[0], this.bottom[1] - 1],
-            [this.bottom[0], this.bottom[1]]
-          ];
-          break;
-        case 270:
-          this.coords = [
-            [this.bottom[0] + 1, this.bottom[1] - 1],
-            [this.bottom[0], this.bottom[1] - 1],
-            [this.bottom[0], this.bottom[1]],
-            [this.bottom[0] - 1, this.bottom[1]]
-          ];
-          break;
-        default:
-          this.coords = [
-            [this.bottom[0] - 1, this.bottom[1]],
-            [this.bottom[0], this.bottom[1]],
-            [this.bottom[0], this.bottom[1] - 1],
-            [this.bottom[0] + 1, this.bottom[1] - 1]
-          ];
-      }
-    };
-  
-    this.createCoords();
-  }
+  this.val = 1;
+  this.state = 0;
+  this.coords = [];
+  this.bottom = [5, 3];
+
+  this.createCoords = function() {
+    switch(this.state) {
+      case 90:
+        this.coords = [
+          [this.bottom[0] + 1, this.bottom[1] - 2],
+          [this.bottom[0] - 1, this.bottom[1] - 1],
+          [this.bottom[0], this.bottom[1] - 1],
+          [this.bottom[0], this.bottom[1]]
+        ];
+        break;
+      case 180:
+        this.coords = [
+          [this.bottom[0] - 1, this.bottom[1] - 2],
+          [this.bottom[0] - 1, this.bottom[1] - 1],
+          [this.bottom[0], this.bottom[1] - 1],
+          [this.bottom[0], this.bottom[1]]
+        ];
+        break;
+      case 270:
+        this.coords = [
+          [this.bottom[0] + 1, this.bottom[1] - 1],
+          [this.bottom[0], this.bottom[1] - 1],
+          [this.bottom[0], this.bottom[1]],
+          [this.bottom[0] - 1, this.bottom[1]]
+        ];
+        break;
+      default:
+        this.coords = [
+          [this.bottom[0] - 1, this.bottom[1]],
+          [this.bottom[0], this.bottom[1]],
+          [this.bottom[0], this.bottom[1] - 1],
+          [this.bottom[0] + 1, this.bottom[1] - 1]
+        ];
+    }
+  };
+
+  this.createCoords();
+}
 
