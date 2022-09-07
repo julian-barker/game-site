20ce1a512c2144f366e715b24cc2d688ae197cf7
Cianenry Danan <cianenryd@gmail.com>
2022-09-07 12:16:51 -0700

    Limited max initial submit to 3 charcters and no empty characters.

--- js/tetris.js
+++ js/tetris.js
@@ -117,15 +117,22 @@ function submitScore() {
   } else {
     scores = [];
   }
+
   const initials = $('#initials').value;
 
-  scores.push({initials, score});
-  scores.sort((a, b) => b.score - a.score);
-  localStorage['scores'] = JSON.stringify(scores);
-  const popup = $('#popup');
-  console.log(maybeStored);
-  popup.remove();
-  playButton();
+  if (initials.length > 3){
+    alert('Three characters max!');
+  } else if(initials === null || initials === ''){
+    alert('Please enter initials!');
+  } else{
+    scores.push({initials, score});
+    scores.sort((a, b) => b.score - a.score);
+    localStorage['scores'] = JSON.stringify(scores);
+    const popup = $('#popup');
+    console.log(maybeStored);
+    popup.remove();
+    playButton();
+  }
 }
 
 function playButton() {
